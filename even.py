8ab4a49ea7e34d27bfb1c16219f9731735f654a9
Mohak Manghirmalani <mohmahn@gmail.com>
2017-01-13 17:38:48 +0530

    Day 1. Getting a hang of the syntax.

---
+++ even.py
@@ -0,0 +1,5 @@
+num = input("Enter number: ")
+if num%2==0:
+	print num, 'is even'
+else:
+	print num, 'is odd'
---
+++ kilo-mile.py
@@ -0,0 +1,3 @@
+x = input("Enter distance in kilometers: ")
+y = x * 0.621371
+print x, 'kilometers = ', y, 'miles'
