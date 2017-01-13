f20415f1297bbc8055802096f367f2ce0b0ba871
Mohak Manghirmalani <mohmahn@gmail.com>
2017-01-13 17:40:25 +0530

    Delete sqroot.py

--- sqroot.py
+++
@@ -1,6 +0,0 @@
-num = input("Enter number you want to calculate square root ")
-sqroot = num**0.5
-print 'Square root of' , num, '=', sqroot
-"""Take input of power and 
-replace it with 0.5 to 
-generalise the result."""
