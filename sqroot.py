01e90f25e9dff5c82a2a98c9b3b6bfa5db2b40d0
Mohak Manghirmalani <mohmahn@gmail.com>
2017-01-13 17:42:13 +0530

    Day 1. Getting a hang of the syntax.

---
+++ sqroot.py
@@ -0,0 +1,6 @@
+num = input("Enter number you want to calculate square root ")
+sqroot = num**0.5
+print 'Square root of' , num, '=', sqroot
+"""Take input of power and 
+replace it with 0.5 to 
+generalise the result."""
