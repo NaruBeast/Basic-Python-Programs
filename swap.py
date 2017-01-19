02dbb0cfb92664562227b08d9578ee8fb0c21f71
Mohak Manghirmalani <mohmahn@gmail.com>
2017-01-19 22:30:57 +0530

    Day 2.

---
+++ swap.py
@@ -0,0 +1,5 @@
+num1 = input("Enter 1st number: ")
+num2 = input("Enter 2nd number: ")
+print "Before swapping: num1 =", num1, "and num2 =", num2 
+num1, num2 = num2, num1
+print "After swapping swapping: num1 =", num1, "and num2 =", num2
