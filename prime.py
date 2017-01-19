acfaa983821271c7c7582f8454e46f4c6ea82171
Mohak Manghirmalani <mohmahn@gmail.com>
2017-01-19 22:33:46 +0530

    Day 2.

---
+++ prime.py
@@ -0,0 +1,14 @@
+x = input("Enter number: ")
+count = 0
+
+if x==1:
+	print "Neither prime nor composite"
+else:	
+	for i in range(1, (x+1)):
+		if x%i == 0:
+			count+=1
+	
+	if count == 2:
+		print 'Prime number'
+	else:
+		print 'Not a prime number'
